1fcfd278e951dbf73a501ebaac444295d07a55c2
brucecham <brucecham@qq.com>
2016-08-29 23:41:55 +0800

    Update app.js

--- app.js
+++ app.js
@@ -39,6 +39,17 @@ app.use(multer({
     return filename;
   }
 }));
+
+//解决跨域
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By",' 3.2.1')
+    res.header("Content-Type", "application/json;charset=utf-8");
+    next();
+});
+
 routes(app);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
